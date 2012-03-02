52784bf1ca188eedf3d1ed6796a8cae79607f71a
Sergii Torgonskyi <2serga2@gmail.com>
2012-03-02 14:35:21 +0200

    Brand new commit

--- client.js
+++ client.js
@@ -1,35 +1,61 @@
-
-strings = {
-	'connected': '[sys][time]%time%[/time]: You\'ve connected to the server as [user]%name%[/user].[/sys]',
-	'messageSent': '[out][time]%time%[/time]: [user]%name%[/user]: %text%[/out]',
-	'messageReceived': '[in][time]%time%[/time]: [user]%name%[/user]: %text%[/in]',
-	'messageFromServer': 'Please, answer, how much is: [serverQuery]'
+var strings = {
+    'connected':'[sys][time]%time%[/time]: You\'ve connected to the server as [user]%name%[/user].[/sys]',
+    'messageSent':'[out][time]%time%[/time]: [user]%name%[/user]: %text%[/out]',
+    'messageReceived':'[in][time]%time%[/time]: [user]%name%[/user]: %text%[/in]',
+    "questionFromServer":'Question id: [questionId]. Please, answer, how much is: [serverQuestion]'
 };
-window.onload = function() {
-	if (navigator.userAgent.toLowerCase().indexOf('chrome') != -1) {
-		socket = io.connect('http://localhost:8080', {'transports': ['xhr-polling']});
-	} else {
-		socket = io.connect('http://localhost:8080');
-	}
-	socket.on('connect', function () {
-		socket.on('message', function (msg) {
-            // Replacing messages with regexp
-			document.querySelector('#log').innerHTML += strings[msg.event].replace(/\[serverQuery\]/, msg.message).replace(/\[([a-z]+)\]/g, '<span class="$1">').replace(/\[\/[a-z]+\]/g, '</span>').replace(/\%time\%/, msg.time).replace(/\%name\%/, msg.name).replace(/\%text\%/, unescape(msg.text).replace('<', '&lt;').replace('>', '&gt;')) + '<br>';
-			// Scrolling the log
-			document.querySelector('#log').scrollTop = document.querySelector('#log').scrollHeight;
-		});
-		// Send text on <Enter> or button press
-		document.querySelector('#input').onkeypress = function(e) {
-			if (e.which == '13') {
-				// Send the escaped value of input
-				socket.send(escape(document.querySelector('#input').value));
-				// Clear the input field
-				document.querySelector('#input').value = '';
-			}
-		};
-		document.querySelector('#send').onclick = function() {
-			socket.send(escape(document.querySelector('#input').value));
-			document.querySelector('#input').value = '';
-		};		
-	});
+
+var userId = 222; //Configured user ID
+
+var generateAnswer = function (quest) {
+    var answer;
+    if (quest.event == 'questionFromServer') {
+        answer = eval(quest.message);
+        console.log(answer);
+        quest.message = answer;
+    } else {
+        console.log('Cannot answer to non-question JSON!');
+        return false;
     }
+    return quest;
+};
+
+window.onload = function () {
+    if (navigator.userAgent.toLowerCase().indexOf('chrome') != -1) {
+        socket = io.connect('http://localhost:8080', {'transports':['xhr-polling']});
+    } else {
+        socket = io.connect('http://localhost:8080');
+    }
+    socket.on('connect', function () {
+        socket.emit('userGreet', userId, function () {
+            console.log('userId was sent to server')
+        });
+
+        socket.on('message', function (msg) {
+            // Replacing messages with regexp
+            document.querySelector('#log').innerHTML += strings[msg.event].replace(/\[questionId\]/, msg.questionId).replace(/\[serverQuestion\]/, msg.message).replace(/\[([a-z]+)\]/g, '<span class="$1">').replace(/\[\/[a-z]+\]/g, '</span>').replace(/\%time\%/, msg.time).replace(/\%name\%/, msg.name).replace(/\%text\%/, unescape(msg.text).replace('<', '&lt;').replace('>', '&gt;')) + '<br>';
+            // Scrolling the log
+            document.querySelector('#log').scrollTop = document.querySelector('#log').scrollHeight;
+
+            // Responding to the server immediately
+            if (msg.event == 'questionFromServer') {
+                console.log(generateAnswer(msg));
+                socket.json.send(msg);
+            }
+        });
+
+        // Send text on <Enter> or button press
+        /*document.querySelector('#input').onkeypress = function(e) {
+         if (e.which == '13') {
+         // Send the escaped value of input
+         socket.send(escape(document.querySelector('#input').value));
+         // Clear the input field
+         document.querySelector('#input').value = '';
+         }
+         };
+        document.querySelector('#send').onclick = function () {
+            socket.send(escape(document.querySelector('#input').value));
+            document.querySelector('#input').value = '';
+        };*/
+    });
+};
--- server.js
+++ server.js
@@ -1,20 +1,84 @@
-var serverMessage = '2+2';
-var verificationMessage = 4;
+var util = require('util');
+var cronJob = require('cron').CronJob;
+var usersScores = new Object();
+var usersQuestions = new Object();
+usersScores['222'] = 0;
+
+function getRandomNumber(maxvalue) {
+    if (arguments.length < 1) {
+        maxvalue = 10;
+    }
+    return Math.floor(Math.random() * (maxvalue + 1));
+}
+
+var generateQuestionId = function (usrId) {
+    var newDate = new Date();
+    return newDate.getTime() + usrId.toString();
+};
+
+var generateQuestionMessage = function () {
+    var trigger = getRandomNumber(1);
+    switch (trigger) {
+        case 0:
+            return getRandomNumber() + ' + ' + getRandomNumber();
+            break;
+        case 1:
+            return getRandomNumber().toString() + ' * ' + getRandomNumber().toString();
+            break;
+        default:
+            return '2+2';
+            break;
+    }
+};
+
+var generateVerification = function (quest) {
+    var verification = eval(quest);
+    console.log(verification);
+    return verification;
+};
+
 
 var io = require('socket.io').listen(8080);
 io.set('log level', 1);
+
 io.sockets.on('connection', function (socket) {
-	var ID = (socket.id).toString().substr(0, 5);
-	var time = (new Date).toLocaleTimeString();
-	socket.json.send({'event': 'connected', 'name': ID, 'time': time});
-	socket.json.send({'event': 'messageFromServer', 'name': ID, 'time': time, 'message': serverMessage});
+    var ID = (socket.id).toString().substr(0, 5);
+    var userId;
+    var time = (new Date).toLocaleTimeString();
+    var questionInterval = 5000;
+
+    socket.on('userGreet', function (usrId) {
+        if (usersScores[usrId] != undefined) {
+            util.log('greetings, ' + usrId);
+            userId = usrId;
+            socket.json.send({'event':'connected', 'name':userId, 'time':time});
+
+            cronJob('* * * * * *', function () {
+                    var questionId = generateQuestionId(userId);
+                    var questionMessage = generateQuestionMessage();
+                    socket.json.send({'event':'questionFromServer', 'name':userId, 'time':time, 'questionId':questionId, 'message':questionMessage});
+                    if (usersQuestions[userId] == undefined) {
+                        usersQuestions[userId] = new Array();
+                    }
+                    usersQuestions[userId][questionId] = new Array();
+
+                    usersQuestions[userId][questionId].push(questionMessage);
+                    usersQuestions[userId][questionId].push(generateVerification(questionMessage));
+                }
+            );
+        } else {
+            util.log('There is no registered users with ID ' + userId);
+        }
+    });
+
     socket.on('message', function (msg) {
         var isCorrect;
-        if (msg){
-            console.log('Message received');
-            isCorrect = (verificationMessage == parseInt(msg));
-		var time = (new Date).toLocaleTimeString();
-		socket.json.send({'event': 'messageSent', 'name': ID, 'text': 'Your answer: ' + msg + '. Correct: ' + isCorrect, 'time': time});
+        if (msg) {
+            util.log('Message received ' + usersQuestions[msg.name][msg.questionId][1]);
+            isCorrect = (msg.message == usersQuestions[msg.name][msg.questionId][1]);
+            var time = (new Date).toLocaleTimeString();
+            socket.json.send({'event':'messageSent', 'name':userId, 'text':'Your answer: ' + msg.message + '. Correct: ' + isCorrect, 'time':time});
         }
-	});
+    });
 });
+
